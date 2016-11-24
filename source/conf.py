1717e368ae24c546baba7622024e65f94d8a6d84
Antti Haapala <antti.haapala@proxion.fi>
2016-11-24 23:47:04 +0200

    added custom css - sidebar on right; configured the theme to use fixed sidebar. Fixed a typo in first steps; added github user+repository data.

--- source/conf.py
+++ source/conf.py
@@ -143,7 +143,11 @@ html_theme = 'alabaster'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'github_user': 'sopython',
+    'github_repo': 'pythonbook',
+    'fixed_sidebar': True
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 # html_theme_path = []
